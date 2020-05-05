87fc96b446f7344ea8278717715dd47389ba1edc
cschaefer26 <c.schaefer.home@gmail.com>
2020-05-05 13:15:21 +0200

    Fixed val.

--- trainer/forward_trainer.py
+++ trainer/forward_trainer.py
@@ -136,7 +136,7 @@ class ForwardTrainer:
         for i, (x, m, ids, lens, dur) in enumerate(val_set, 1):
             x, m, dur, lens = x.to(device), m.to(device), dur.to(device), lens.to(device)
             with torch.no_grad():
-                m1_hat, m2_hat, dur_hat = model(x, m, dur)
+                m1_hat, m2_hat, dur_hat = model.gen(x, m, dur)
                 m1_loss = self.l1_loss(m1_hat, m, lens)
                 m2_loss = self.l1_loss(m2_hat, m, lens)
                 dur_loss = F.l1_loss(dur_hat, dur)
