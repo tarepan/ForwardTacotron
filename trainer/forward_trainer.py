c33fa17138c5369f1dfea8814a255c57458c5327
cschaefer26 <c.schaefer.home@gmail.com>
2020-08-20 13:12:35 +0200

    Added autoalignment stuff from branch.

--- trainer/forward_trainer.py
+++ trainer/forward_trainer.py
@@ -113,8 +113,8 @@ class ForwardTrainer:
             x, m, dur, lens = x.to(device), m.to(device), dur.to(device), lens.to(device)
             with torch.no_grad():
                 m1_hat, m2_hat, dur_len_hat = model(x, m, lens)
-                m1_loss = self.l1_loss(m1_hat, m, lens)
-                m2_loss = self.l1_loss(m2_hat, m, lens)
+                m1_loss = F.l1_loss(m1_hat, m)
+                m2_loss = F.l1_loss(m2_hat, m)
                 m_val_loss += m1_loss.item() + m2_loss.item()
         return m_val_loss / len(val_set)
 
