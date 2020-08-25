e7b6bff55c398ec9df851c89d8bd4ae4b886794f
cschaefer26 <c.schaefer.home@gmail.com>
2020-08-25 12:03:08 +0200

    With offset.

--- models/forward_tacotron.py
+++ models/forward_tacotron.py
@@ -139,7 +139,7 @@ class ForwardTacotron(nn.Module):
         self.embedding = nn.Embedding(num_chars, embed_dims)
         self.lr = LengthRegulator()
         self.dur_pred = DurationPredictor(256, conv_dims=durpred_conv_dims)
-        self.prenet = ConvStack(embed_dims, layers=10)
+        self.prenet = ConvStack(embed_dims, layers=2)
 
         self.lin = torch.nn.Linear(2 * rnn_dim, n_mels)
         self.register_buffer('step', torch.zeros(1, dtype=torch.long))
