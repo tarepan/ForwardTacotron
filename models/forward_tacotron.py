1cd62b2f9e9fde672cb186b888959df653edacc2
cschaefer26 <c.schaefer.home@gmail.com>
2021-07-07 11:18:32 +0200

    Testing align.

--- models/forward_tacotron.py
+++ models/forward_tacotron.py
@@ -1,11 +1,12 @@
 from pathlib import Path
+from random import random
 from typing import Union, Callable, Dict, Any
 
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn import Embedding
+from torch.nn import Embedding, GRU, Linear
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence, pad_sequence
 
 from models.common_layers import CBHG
@@ -20,6 +21,8 @@ class LengthRegulator(nn.Module):
     def forward(self, x: torch.Tensor, dur: torch.Tensor) -> torch.Tensor:
         x_expanded = []
         for i in range(x.size(0)):
+            print(x[i].size())
+            print(dur[i].size())
             x_exp = torch.repeat_interleave(x[i], (dur[i] + 0.5).long(), dim=0)
             x_expanded.append(x_exp)
         x_expanded = pad_sequence(x_expanded, padding_value=0., batch_first=True)
@@ -141,11 +144,14 @@ class ForwardTacotron(nn.Module):
         self.energy_strength = energy_strength
         self.pitch_proj = nn.Conv1d(1, 2 * prenet_dims, kernel_size=3, padding=1)
         self.energy_proj = nn.Conv1d(1, 2 * prenet_dims, kernel_size=3, padding=1)
+        self.att_rnn = GRU(6*prenet_dims, 64, bidirectional=True, batch_first=True)
+        self.att_lin = nn.Linear(128, 3)
 
     def forward(self, batch: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
         x = batch['x']
         mel = batch['mel']
         dur = batch['dur']
+
         mel_lens = batch['mel_len']
         pitch = batch['pitch'].unsqueeze(1)
         energy = batch['energy'].unsqueeze(1)
@@ -169,7 +175,30 @@ class ForwardTacotron(nn.Module):
         energy_proj = energy_proj.transpose(1, 2)
         x = x + energy_proj * self.energy_strength
 
-        x = self.lr(x, dur)
+        x_copy = x
+        x_mid = self.lr(x_copy, dur)
+        x_right = self.lr(x_copy[:, 1:, :], dur[:, :-1])
+        x_left = self.lr(torch.cat(
+            [torch.zeros(x_copy[:, :1, :].size()).to(x.device), x_copy[:, :-1, :]], dim=1), dur)
+
+        print(x[0, :20, 0])
+        print(x_right[0, :20, 0])
+        print(x_left[0, :20, 0])
+
+        min_len = min(x_left.size(1), x_mid.size(1), x_right.size(1))
+        x_left = x_left[:, :min_len, :]
+        x_mid = x_mid[:, :min_len, :]
+        x_right = x_right[:, :min_len, :]
+
+        x_att_in = torch.cat([x_left, x_mid, x_right], dim=-1)
+        x_att, _ = self.att_rnn(x_att_in)
+        x_att = self.att_lin(x_att)
+        x_att = torch.softmax(x_att, dim=-1)
+        print(x_left.size())
+        print(x_att.size())
+        x = x_left * x_att[:, :, 0:1] + x_mid * x_att[:, :, 1:2] + x_right * x_att[:, :, 2:3]
+        if torch.rand(1)[0] < 0.1:
+            print(x_att[0, :100, :])
 
         x = pack_padded_sequence(x, lengths=mel_lens.cpu(), enforce_sorted=False,
                                  batch_first=True)
