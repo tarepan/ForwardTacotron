836d975a7456a866a22b8727f688cfe8bdd8a149
cschaefer26 <c.schaefer.home@gmail.com>
2021-10-12 12:03:11 +0200

    ctc_attention.

--- trainer/taco_trainer.py
+++ trainer/taco_trainer.py
@@ -78,7 +78,7 @@ class TacoTrainer:
                 model.train()
                 k = step // 1000
 
-                if step < 10:
+                if step < 10000:
                     ctc_out = model.aligner(batch['mel'].transpose(1, 2))
                     ctc_out = ctc_out.transpose(0, 1).log_softmax(2)
                     loss_ctc = self.ctc_loss(ctc_out, batch['x'], batch['mel_len'], batch['x_len'])
